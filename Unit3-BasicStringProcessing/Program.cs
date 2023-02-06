ba8c9e59c6d694463f16a82cb359f9f021b53175
nikjk <nik.kuehn@outlook.com>
2023-02-05 22:31:49 -0500

    Add project files.

---
+++ Unit3-BasicStringProcessing/Program.cs
@@ -0,0 +1,50 @@
+﻿bool doAgain = false;
+do
+{
+    Console.Write("Please enter a sentance:");
+    string userSentance = Console.ReadLine();
+    string[] words = userSentance.Split(' ');
+
+    foreach (string word in words)
+    {
+        Console.WriteLine(word);
+    }
+
+    Console.WriteLine("Would you like to go again (y/n)?");
+    string userInput = Console.ReadLine();
+    if (userInput == "y")
+    {
+        doAgain= true;
+    }
+    else
+    {
+        doAgain= false;
+    }
+} while (doAgain == true);
+
+doAgain= false;
+List<string> strings= new List<string>();
+Console.WriteLine("\n\n");
+do
+{
+    Console.WriteLine("Enter some text: ");
+    strings.Add(Console.ReadLine());
+    Console.WriteLine("You have entered: ");
+    foreach (string word in strings)
+    {
+        Console.Write(word + " ");
+    }
+    Console.WriteLine("\n");
+    Console.WriteLine("Would you like to continue (y/n)?");
+    string userInput = Console.ReadLine();
+
+    if (userInput == "y")
+    {
+        doAgain = true;
+    }
+    else
+    {
+        doAgain = false;
+    }
+} while (doAgain == true);
+Console.WriteLine("Goodbye!");
